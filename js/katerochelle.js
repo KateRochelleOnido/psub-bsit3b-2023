35f73809caae46d14b06024e174c0ae29a93080a
Kate Rochelle Onido <katonido@my.cspc.edu.ph>
2023-01-31 10:04:20 +0800

    Add Kate Rochelle Onido

---
+++ js/katerochelle.js
@@ -0,0 +1,10 @@
+devs.push({
+
+    firstName : 'Kate Rochelle',
+    middleName: 'Baluis',
+    lastName  : 'Onido',
+    gender    : '[Female]',
+    username  : 'Kate Rochelle Onido',
+    photo     : 'YourGitHubAvatarLink'
+
+});
